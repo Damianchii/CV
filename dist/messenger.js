d15166ce75b8f6bb49d423809569f4bf286b14e9
Damianchii <damianczerwinsky@gmail.com>
2023-10-24 20:48:23 +0200

    Doodano pliki

--- dist/messenger.js
+++ dist/messenger.js
@@ -1,65 +1,104 @@
 const chatMessages = [
 	{
 		name: 'me',
-		message: 'Lorem ipsum',
-		delay: 1000,
+		message: "Hello I'm Damian Czerwiński, a passionate Front-end Developer based in Kielce Poland. I'm 23 years old.",
 		align: 'right',
-		showTime: true,
-		time: '19:58', //dodac prawidzwy czas
 	},
 	{
 		name: 'chef',
-		message: 'Lorem ipsum',
-		delay: 6000,
-		align: 'left',
-		showTime: true,
-		time: '19:58', //dodac prawidzwy czas
+		message: 'I want to express my interests in a position at your company',
+		align: 'right',
 	},
 	{
 		name: 'me',
 		message: 'dLorem ipsum',
-		delay: 3000,
 		align: 'right',
-		showTime: true,
-		time: '19:58', //dodac prawidzwy czas
 	},
 	{
 		name: 'chef',
 		message: 'Lorem ipsum',
-		delay: 6000,
 		align: 'right',
-		showTime: true,
-		time: '19:58', //dodac prawidzwy czas
 	},
 	{
 		name: 'chef',
 		message: 'Lorem ipsum',
-		delay: 6000,
-		align: 'right',
-		showTime: true,
-		time: '19:58', //dodac prawidzwy czas
+		align: 'left',
 	},
 	{
 		name: 'chef',
 		message: 'Lorem ipsum',
-		delay: 6000,
 		align: 'right',
-		showTime: true,
-		time: '19:58', //dodac prawidzwy czas
 	},
 ]
 //Current time
 let dt = new Date()
-let time = dt.getHours() + ':' + dt.getMinutes()
+
+const minutes = () => {
+	let minutesUnderTen = '0'
+	if (dt.getMinutes() < 10) {
+		minutesUnderTen = minutesUnderTen + dt.getMinutes()
+		return minutesUnderTen
+	} else {
+		return dt.getMinutes()
+	}
+}
+
+// po wywolaniu schodzi na dol diva
+const updateScroll = () => {
+	let element = document.querySelector('#main-container-messages')
+	element.scrollTop = element.scrollHeight
+}
+
+//pobranie i wyswietlenie czasu
+let time = dt.getHours() + ':' + minutes()
 $('#time').append(time)
 
-let chatDelay = 0
+let number = 0
 
-$.each(chatMessages, function (index, element) {
-	$('#chat-meggase-list').append(`<li class="flex rounded-full justify-${element.align == 'right' ? 'end' : 'start'}">
-    <p id="message-padding-${element.align == 'right' ? 'right' : 'left'}"
-        class="max-w-[250px] bg-[#0078FF] text-white py-2 px-4 break-all">
-        ${element.message}
-    </p>
-</li>`)
-})
+const waitingForMessage = () => {
+	$('#chat-meggase-list').append(`<li class="flex rounded-full justify-${
+		chatMessages[number].align == 'right' ? 'end' : 'start'
+	} animate-[fade-in-message-${chatMessages[number].align == 'right' ? 'right' : 'left'}_0.5s_ease]">
+	<p id="message-padding-${chatMessages[number].align == 'right' ? 'right' : 'left'}"
+		class="flex justify-center items-center gap-1 max-w-[250px] bg-[#303030] text-white py-2 px-4 h-[40px]">
+		<span
+			class="w-2 h-2 rounded-full bg-[#aaa] animate-[while-writing-animation_1.5s_linear_infinite_both] delay-1000"></span>
+		<span
+			class="w-2 h-2 rounded-full bg-[#aaa] animate-[while-writing-animation_1.5s_linear_infinite_both_0.2s]"></span>
+		<span
+			class="w-2 h-2 rounded-full bg-[#aaa] animate-[while-writing-animation_1.5s_linear_infinite_both_0.4s]"></span></p></li>`)
+	updateScroll()
+}
+
+waitingForMessage()
+
+let intervalID = setInterval(() => {
+	// sprawdzenie, jesli number jest wiekszy od elemetow listy interval zakancza sie
+	if (number >= chatMessages.length - 1) {
+		clearInterval(intervalID)
+	}
+	//usun animacje pisania wiadomosci
+	const menu = document.querySelector('#chat-meggase-list')
+	menu.removeChild(menu.lastElementChild)
+
+	//dodawanie elementu
+	$('#chat-meggase-list').append(`<li class="flex rounded-full animate-[fade-in-message-${
+		chatMessages[number].align == 'right' ? 'right' : 'left'
+	}_0.5s_ease] justify-${chatMessages[number].align == 'right' ? 'end' : 'start'}">
+	    <p id="message-padding-${chatMessages[number].align == 'right' ? 'right' : 'left'}"
+	        class="max-w-[250px] bg-[${
+						chatMessages[number].align == 'right' ? '#0085FE' : '#444444' //0085FE
+					}] text-white py-2 px-4 break-all">
+	        ${chatMessages[number].message}
+	    </p>
+	</li>`)
+
+	number++
+
+	//wykonuj animacje pisania wiadomosci
+	if (number >= chatMessages.length) {
+		return
+	} else {
+		setTimeout(waitingForMessage(), 3000)
+	}
+}, 3000)
